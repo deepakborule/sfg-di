c7feeb42660361ba19f9e3b7572bfde392703ed2
Ved Borule <vedborule@gmail.com>
2021-06-26 11:39:47 +0530

    Added Default to Profile

--- src/main/java/com/springframework/sfgdi/services/I18nEnglishGreetingServiceImpl.java
+++ src/main/java/com/springframework/sfgdi/services/I18nEnglishGreetingServiceImpl.java
@@ -4,14 +4,16 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("ES")
+
+// If we set default then if no profile is found this will act as default profile
+@Profile({"ES","default"})
 @Service("i18nService")
 public class I18nEnglishGreetingServiceImpl implements GreetingService {
 
 	@Override
 	public String sayGreeting() {
 		
-		return "Hola Hano-ES";
+		return "Hola Mundo-ES";
 	}
 
 }
